5b1e1ab88bc710cc8f594ded7c5e938e66924d12
Sad Pencil <qh06@qq.com>
2019-07-09 15:31:23 +0800

    add go-kit/kit as the new logger log the time that client receive a new transaction

--- log2/logger.go
+++ log2/logger.go
@@ -3,16 +3,14 @@ package log2
 import (
 	"encoding/json"
 	"fmt"
+	kitlog "github.com/go-kit/kit/log"
+	"io"
 	"os"
 	"time"
-
-	"geth-timing/go-logging"
 )
 
-var logger = logging.MustGetLogger("timing")
-var format = logging.MustStringFormatter(
-	`%{message}`,
-)
+//var logger kitlog.Logger
+var syncWriter io.Writer
 
 func InitOutputFile(outputFile string) {
 
@@ -20,6 +18,7 @@ func InitOutputFile(outputFile string) {
 		_, _ = fmt.Fprintln(os.Stderr, "You must specify the log file, e.g. \"geth --timing.output=/path/to/file.txt\"")
 		os.Exit(1)
 	}
+
 	var err error
 	file, err := os.OpenFile(outputFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
@@ -29,23 +28,9 @@ func InitOutputFile(outputFile string) {
 		os.Exit(1)
 	}
 
-	backend := logging.NewLogBackend(file, "", 0)
-	formatter := logging.NewBackendFormatter(backend, format)
-	logging.SetBackend(formatter)
+	syncWriter = kitlog.NewSyncWriter(file)
 
 	_ = Record(map[string]interface{}{"Message": "Timing log initialized.", "Type": "Message"})
-	//_, err = file.WriteString("Timing log initialized.\n")
-	//if err != nil {
-	//	_, _ = fmt.Fprintln(os.Stderr, "Failed to write log file for timing.")
-	//	_, _ = fmt.Fprintln(os.Stderr, err)
-	//	os.Exit(1)
-	//}
-	//err = file.Sync()
-	//if err != nil {
-	//	_, _ = fmt.Fprintln(os.Stderr, "Failed to write log file for timing.")
-	//	_, _ = fmt.Fprintln(os.Stderr, err)
-	//	os.Exit(1)
-	//}
 }
 
 func Record(timingLog map[string]interface{}) error {
@@ -56,15 +41,12 @@ func Record(timingLog map[string]interface{}) error {
 		return err
 	}
 
-	logger.Info(string(b))
+	b = append(b, '\n')
+
+	_, err = syncWriter.Write(b)
+	if err != nil {
+		return err
+	}
+
 	return nil
-	//b = append(b, '\n')
-	//
-	//_, err = file.Write(b)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//err = file.Sync()
-	//return err
 }
